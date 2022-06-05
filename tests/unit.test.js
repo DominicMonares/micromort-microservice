594f68b348b78cde731f03d1d07faf238b204dc4
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-05 04:51:18 -0700

    update validation helpers

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,25 +1,25 @@
 const {
-  commuterIDValid,
-  timestampsValid,
-  actionsValid,
-  unitsValid,
-  quantitiesValid
-} = require('../helpers/verification.js');
+  validateCommuterID,
+  validateTimestamps,
+  validateActions,
+  validateUnits,
+  validateQuantities
+} = require('../helpers/validation.js');
 
 describe('Risk in Micromorts - Unit', () => {
 
   describe('Commuter IDs', () => {
 
     it('should return true for valid commuter IDs', () => {
-      expect(commuterIDValid('COM-1')).toBe(true);
+      expect(validateCommuterID('COM-1')).toBe(true);
     });
 
     it('should return false for invalid commuter ID numbers', () => {
-      expect(commuterIDValid('COM-Z')).toBe(false);
+      expect(validateCommuterID('COM-Z')).toBe(false);
     });
 
     it('should return false for invalid commuter ID prefixes', () => {
-      expect(commuterIDValid('MOC-1')).toBe(false);
+      expect(validateCommuterID('MOC-1')).toBe(false);
     });
 
   });
@@ -28,12 +28,12 @@ describe('Risk in Micromorts - Unit', () => {
 
     it('should return true for valid timestamps', () => {
       const timestamps = ['2022-01-01 10:05:11', '2022-01-01 10:16:52'];
-      expect(timestampsValid(timestamps)).toBe(true);
+      expect(validateTimestamps(timestamps)).toBe(true);
     });
 
     it('should return false for invalid timestamps', () => {
       const timestamps = ['2022-01-01 10:05:11', '2022-01-11 10:16:52'];
-      expect(timestampsValid(timestamps)).toBe(false);
+      expect(validateTimestamps(timestamps)).toBe(false);
     });
 
   });
@@ -42,12 +42,12 @@ describe('Risk in Micromorts - Unit', () => {
 
     it('should return true for valid actions', () => {
       const actions = ['surfed through lava', 'unicycled in bike lane'];
-      expect(actionsValid(actions)).toBe(true);
+      expect(validateActions(actions)).toBe(true);
     });
 
     it('should return false for invalid actions', () => {
       const actions = ['swam through cola', 123];
-      expect(actionsValid(actions)).toBe(false);
+      expect(validateActions(actions)).toBe(false);
     });
 
   });
@@ -56,12 +56,12 @@ describe('Risk in Micromorts - Unit', () => {
 
     it('should return true for valid units', () => {
       const units = ['mile', 'minute', 'floor', 'quantity'];
-      expect(unitsValid(units)).toBe(true);
+      expect(validateUnits(units)).toBe(true);
     });
 
     it('should return false for invalid units', () => {
       const units = ['mile', 'minute', 'parsec', 'quantity'];
-      expect(unitsValid(units)).toBe(false);
+      expect(validateUnits(units)).toBe(false);
     });
 
   });
@@ -70,12 +70,12 @@ describe('Risk in Micromorts - Unit', () => {
 
     it('should return true for valid quantities', () => {
       const quantities = [54, 63];
-      expect(quantitiesValid(quantities)).toBe(true);
+      expect(validateQuantities(quantities)).toBe(true);
     });
 
     it('should return true for valid quantities', () => {
       const quantities = [54, '63'];
-      expect(quantitiesValid(quantities)).toBe(true);
+      expect(validateQuantities(quantities)).toBe(true);
     });
 
   });
