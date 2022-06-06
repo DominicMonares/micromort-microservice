3feef8c53e85b892e6b0a8bea4e646fa45dc605f
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-05 18:57:59 -0700

    implement validation functions

--- helpers/validation.js
+++ helpers/validation.js
@@ -1,31 +1,36 @@
-const validateCommuterID = (commuterID) => {
-  if (typeof id !== 'string' || commuterID.split('-').length !== 2) {
-    return false;
-  }
+const { dateValid, timeValid } = require('./timestamps.js');
+const { unitRules } = require('../rules.js');
 
-  const id = commuterID.split('-');
-  const validNumber = Number(id[1]) > 0;
-  if (id[0] === 'COM' && validNumber) {
-    return true;
-  } else {
-    return false;
-  }
+const validateCommuterID = (id) => {
+  if (typeof id !== 'string' || id.split('-').length !== 2) { return false }
+
+  id = id.split('-');
+  id[0] === 'COM' && Number(id[1] > 0) ? true : false;
 }
 
-const validateTimestamps = () => {
+const validateTimestamps = (timestamps) => {
+  return timestamps.every(t => {
+    if (typeof t !== 'string' || t.split(' ').length !== 2) { return false }
 
+    t = t.split(' ');
+    dateValid(t[0]) && timeValid(t[1]) ? true : false;
+  });
 }
 
-const validateActions = () => {
-
+const validateActions = (actions) => {
+  return actions.every(a => typeof actions === 'string');
 }
 
-const validateUnits = () => {
-
+const validateUnits = (units) => {
+  return units.every(u => {
+    unitRules[u] ? true : false;
+  })
 }
 
-const validateQuantities = () => {
-
+const validateQuantities = (quantities) => {
+  return quantities.every(q => {
+    typeof q !== 'number' || q <= 0 ? false : true;
+  });
 }
 
 module.exports = {
