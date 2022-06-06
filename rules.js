b11e6569d9537d0d280864d0106188c173e196bc
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-05 17:29:12 -0700

    implement MMDD and unit rules

---
+++ rules.js
@@ -0,0 +1,24 @@
+// implement date validation for current day?
+// i.e. rejecting dates/times later than current day/time
+
+const mmDDRules = {
+  1: 31,
+  2: 29,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31
+}
+
+const unitRules = {
+  mile: true,
+  floor: true,
+  minute: true,
+  quantity: true
+}
