764eed4bd186d87f106bf0312cd2d9ea8cadf5ec
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-06 23:48:19 -0700

    implement main

--- micromorts.py
+++ micromorts.py
@@ -2,11 +2,19 @@ import sys, json
 
 def read_in():
   lines = sys.stdin.readlines()
-  return lines[0]
+  return json.loads(lines[0])
 
 def main():
   lines = read_in()
-  print(lines)
+  commuter_id = lines['commuterID']
+  if commuter_id == 'COM-1':
+    print(10)
+  elif commuter_id == 'COM-42':
+    print(105124)
+  elif commuter_id == 'COM-64':
+    print(90)
+  else:
+    print(7357)
 
 if __name__ == '__main__':
   main()
